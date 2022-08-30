8aa2f7ae4335cd515bafd02ddd4f72d6d76337f1
r-ILIASS <iliass.root@gmail.com>
2022-08-30 11:44:21 +0100

    docs: important documentation

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
+        // TODO: remove || !origin rule for production
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -10,11 +10,10 @@ const verifyRoles = (...allowedRoles) => {
     console.log("ROLES ARRAY: ", rolesArray);
     console.log("REQ ROLES: ", req.roles);
 
-    // compare roles
+    // compare roles and find one "true" value
     const result = req.roles
-      .map((role) => rolesArray.includes(role))
-      .find((val) => val === true);
-
+      .map((role) => rolesArray.includes(role)) // ex: [true, false, false]
+      .find((val) => val === true); // finds one true value
     if (!result) {
       console.log("** YOU DON'T HAVE THE ROLE TO DO THIS ACTION!"); // TODO: remove in prodoction
       return res.sendStatus(401);
