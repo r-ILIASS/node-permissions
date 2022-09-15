174d1b32f2a82e79c400598180b2645b6a97a8ad
r-ILIASS <iliass.root@gmail.com>
2022-09-15 11:06:44 +0100

    minor

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,7 +2,9 @@ const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+  if (!authHeader?.startsWith("Bearer ")) return res.status(401).json({
+    message: "No Bearer token provided"
+  });
   console.log("BEARER TOKEN IN VERIFYJWT()", authHeader); // Bearer token TODO: remove in production
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
